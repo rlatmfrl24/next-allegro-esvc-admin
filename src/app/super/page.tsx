ed1c9efecde55aee03d7dbf21edfc8072702c95f
SulfurBottom <397love@gmail.com>
2024-03-27 14:33:18 +0900

    Add dependencies and update SuperHome component

--- src/app/super/page.tsx
+++ src/app/super/page.tsx
@@ -1,21 +1,122 @@
 "use client";
 
-import { MdFilledButton } from "@/util/md3";
+import {
+  MdFilledButton,
+  MdIcon,
+  MdIconButton,
+  MdRippleEffect,
+  MdSwitch,
+} from "@/util/md3";
+import { MdTypography } from "../components/typography";
+import { MoreVert } from "@mui/icons-material";
+import { DividerComponent } from "../components/divider";
+import EserviceLogo from "@/../public/logo_esvc.svg";
+import TransfarShippingLogo from "@/../public/logo_transfar_shipping.svg";
+import CyberlogitecLogo from "@/../public/logo_cyberlogitec.svg";
+import TSLineLogo from "@/../public/logo_tsline.svg";
+import Image from "next/image";
 
 export default function SuperHome() {
   return (
     <div className="flex-1 flex flex-col p-8">
       <MdFilledButton className="w-fit self-end">Create Company</MdFilledButton>
-      <div className="grid grid-cols-4"> </div>
+      <div className="grid grid-cols-4 gap-6 mt-6 ">
+        <CompanyCard
+          companyName="E-Service"
+          companyCode="ESVC"
+          companyLogo={<Image src={EserviceLogo} alt="E-Service Logo" />}
+          isActivated={true}
+          isTemporarySaved
+        />
+        <CompanyCard
+          companyName="Transfar Shipping"
+          companyCode="TFSH"
+          companyLogo={
+            <Image src={TransfarShippingLogo} alt="Transfar Shipping Logo" />
+          }
+          isActivated={true}
+        />
+        <CompanyCard
+          companyName="Cyberlogtiec"
+          companyCode="CBLT"
+          companyLogo={<Image src={CyberlogitecLogo} alt="Cyberlogtiec Logo" />}
+        />
+        <CompanyCard
+          companyName="TS Line"
+          companyCode="TSLN"
+          companyLogo={<Image src={TSLineLogo} alt="TS Line Logo" />}
+          isActivated={true}
+        />
+      </div>
     </div>
   );
 }
 
-const CompanyCard = ({}: {
+const CompanyCard = (props: {
   companyName: string;
   companyCode: string;
-  isActivated: boolean;
+  isActivated?: boolean;
+  isTemporarySaved?: boolean;
   companyLogo?: React.ReactNode;
 }) => {
-  return <div className="h-64"></div>;
+  return (
+    <div
+      className="h-72 rounded-lg flex flex-col bg-surfaceContainerLowest border-2 border-secondaryContainer overflow-hidden relative cursor-pointer "
+      onClick={() => {
+        console.log("Company Card Clicked");
+      }}
+    >
+      <div className="bg-surfaceContainerLow h-14 flex justify-between items-center pl-4 pr-2">
+        <MdTypography variant="body" size="medium" prominent>
+          {props.companyName}
+        </MdTypography>
+        <div className="flex items-center">
+          {props.isTemporarySaved && (
+            <MdTypography
+              variant="label"
+              size="medium"
+              className="bg-errorContainer text-onErrorContainer px-2 py-0.5 rounded-lg"
+            >
+              Temporary Saved
+            </MdTypography>
+          )}
+          <MdIconButton
+            onClick={(e) => {
+              e.stopPropagation();
+            }}
+          >
+            <MdIcon>
+              <MoreVert />
+            </MdIcon>
+          </MdIconButton>
+        </div>
+      </div>
+      <div className="flex-1 flex-col flex justify-center items-center">
+        <MdTypography variant="title" size="medium" className="text-primary">
+          {props.companyCode}
+        </MdTypography>
+        {props.companyLogo && (
+          <div className="h-16 flex items-center">{props.companyLogo}</div>
+        )}
+      </div>
+      <div className="p-4 text-right">
+        <DividerComponent className="mb-4 border-dotted" />
+        <MdTypography
+          variant="label"
+          size="medium"
+          tag="label"
+          className="text-outline"
+        >
+          Active
+          <MdSwitch
+            className="ml-2"
+            selected={props.isActivated}
+            onClick={(e) => {
+              e.stopPropagation();
+            }}
+          />
+        </MdTypography>
+      </div>
+    </div>
+  );
 };
