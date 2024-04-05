5f30a8637effe921d92cdea7426025aff4505882
SulfurBottom <397love@gmail.com>
2024-04-05 16:18:27 +0900

    Add className "z-10" to HeaderComponent and update MessageManagementSetup layout

--- src/app/main/setup/message/page.tsx
+++ src/app/main/setup/message/page.tsx
@@ -4,7 +4,12 @@ import { DateTime } from "luxon";
 import { PageTitle } from "../../components/page-title";
 import NAOutlinedListBox from "@/app/components/na-outline-listbox";
 import { NAOutlinedTextField } from "@/app/components/na-textfield";
-import { MdFilledButton, MdIcon, MdTextButton } from "@/util/md3";
+import {
+  MdFilledButton,
+  MdIcon,
+  MdOutlinedTextField,
+  MdTextButton,
+} from "@/util/md3";
 import { Add } from "@mui/icons-material";
 import { MdTypography } from "@/app/components/typography";
 
@@ -53,9 +58,37 @@ export default function MessageManagementSetup() {
             Total: 0
           </MdTypography>
         </div>
-        <div className="flex">
-          <div>table</div>
-          <div></div>
+        <div className="flex gap-8 flex-1 ">
+          <div className="basis-2/3">table</div>
+          <div className="basis-1/3 px-6 py-4 rounded-lg h-full flex flex-col gap-4 bg-surface border-2 border-secondaryContainer">
+            <MdTypography variant="label" size="large" prominent>
+              Language Detail
+            </MdTypography>
+            <MdOutlinedTextField
+              label="English"
+              className="flex-1 resize-none"
+              type="textarea"
+              placeholder="Notify name, Notify Address is mandatory field."
+            />
+            <MdOutlinedTextField
+              label="Korean"
+              className="flex-1 resize-none"
+              type="textarea"
+              placeholder="이름, 주소는 필수 입력란입니다."
+            />
+            <MdOutlinedTextField
+              label="Japanese"
+              className="flex-1 resize-none"
+              type="textarea"
+              placeholder="通知名、通知先は必須項目です。"
+            />
+            <MdOutlinedTextField
+              label="Chinese"
+              className="flex-1 resize-none"
+              type="textarea"
+              placeholder="通知名称、通知地址为必填字段。"
+            />
+          </div>
         </div>
       </div>
     </div>
