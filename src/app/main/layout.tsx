523d9eade7b3bc55490dc65537a47c667836bcf0
SulfurBottom <397love@gmail.com>
2024-04-24 11:01:31 +0900

    Add BottomFloatingBar component to MainLayout

--- src/app/main/layout.tsx
+++ src/app/main/layout.tsx
@@ -1,6 +1,6 @@
-import { OverlayScrollbarsComponent } from "overlayscrollbars-react";
 import { HeaderComponent } from "./components/header";
 import SideNavigation from "./components/side-nav";
+import { BottomFloatingBar } from "./components/bottom-floating-bar";
 
 export default function MainLayout({
   children,
@@ -10,11 +10,12 @@ export default function MainLayout({
   return (
     <div className="bg-surfaceContainerHigh h-screen flex">
       <SideNavigation />
-      <nav className="bg-surfaceContainerLow flex-1 rounded-3xl flex flex-col">
+      <nav className="relative bg-surfaceContainerLow flex-1 rounded-3xl flex flex-col">
         <HeaderComponent branch="KRPUS" />
         <main className="bg-surfaceContainerLowest flex-1 rounded-3xl px-8 py-6 flex flex-col overflow-auto">
           {children}
         </main>
+        <BottomFloatingBar />
       </nav>
     </div>
   );
